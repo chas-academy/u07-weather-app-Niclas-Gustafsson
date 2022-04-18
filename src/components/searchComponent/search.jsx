d92a2e9a79739764c3126a3b064a0bc81711ae32
Niclas-Gustafsson <niclas.gustafsson@chasacademy.se>
2022-04-18 15:07:50 +0200

    Basic search component styling + add global variables for Sass

--- src/components/searchComponent/search.jsx
+++ src/components/searchComponent/search.jsx
@@ -4,8 +4,11 @@ export default function Search() {
   return (
     <section className="search-box">
       <form className="search-form" action="">
+        <div className="form-group">
+
         <label htmlFor="search">Search location</label>
         <input id="search" type="text" placeholder="Search location"/>
+        </div>
         <button type="submit">Search</button>
       </form>
     </section>
